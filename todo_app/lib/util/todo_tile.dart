044be08eef5c918a41fc05994f0e6e1f05990a23
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-19 02:00:43 +0530

    tile decoration

--- todo_app/lib/util/todo_tile.dart
+++ todo_app/lib/util/todo_tile.dart
@@ -16,7 +16,7 @@ function(bool?)? onChanged;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25,top:25 ),
       child: Container(
         padding: const EdgeInsets.all(24),
         child: Row(children: [

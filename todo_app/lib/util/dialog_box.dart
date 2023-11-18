ed3d108ede6122cdf1bde48d46d3391392481270
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-19 02:20:49 +0530

    new inpt

--- todo_app/lib/util/dialog_box.dart
+++ todo_app/lib/util/dialog_box.dart
@@ -4,6 +4,14 @@ class DialogBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      child : Column(children: [
+        TextField(
+          decoration: InputDecoration(border: OutlineInputBorder(),hintText: "add a new task"),
+        ),
+      ],)
+      content: Container(height: 120),
+    );
   }
 }

5c9e1cd6f1a20217e8c8ee7295ce385376672868
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-19 01:24:13 +0530

    basic ui

---
+++ todo_app/lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        // centerTitle: ,
+        //cente
+        title: const Text('PIYUSH KA TUDU'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: const [
+          ToDoTile(),
+        ],
+
+
+      ),
+    );
+  }
+}
+
+
---
+++ todo_app/lib/util/todo_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  const ToDoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12)
+          ),
+        child: const Text("make tutorial"),
+      )  
+    );
+  }
+}
